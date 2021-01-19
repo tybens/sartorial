a96f9a8651914ac6254e3732be03e07b341cbf3b
tybens <tb19@princeton.edu>
2021-01-18 17:30:25 -0800

    Add cart page

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { Container, Typography, Button, Grid } from "@material-ui/core";
+import useStyles from "./styles";
+import CartItem from "./CartItem/CartItem";
+import { Link } from "react-router-dom";
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
+  const classes = useStyles();
+
+  const EmptyCart = () => (
+    <Typography variant="subtitle1">
+      You have no items in your shopping cart,
+      <Link to="/" className={classes.link}>
+        start adding some!
+      </Link>
+    </Typography>
+  );
+
+  const FilledCart = () => (
+    <>
+      <Grid container spacing={3}>
+        {cart.line_items.map((item) => (
+          <Grid item xs={12} sm={4} key={item.id}>
+            <CartItem
+              item={item}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+            />
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+        <Typography variant="h4">
+          Subtotal: {cart.subtotal.formatted_with_symbol}
+        </Typography>
+        <div>
+          <Button
+            className={classes.emptyButton}
+            size="large"
+            type="button"
+            variant="contained"
+            color="secondary"
+            onClick={handleEmptyCart}
+          >
+            Empty Cart
+          </Button>
+          <Button
+            component={Link}
+            to="/checkout"
+            className={classes.checkoutButton}
+            size="large"
+            type="button"
+            variant="contained"
+            color="primary"
+          >
+            Checkout
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+
+  if (!cart.line_items) return "Loading...";
+
+  return (
+    <Container>
+      <div className={classes.toolbar} />
+      <Typography className={classes.title} variant="h3" gutterBottom>
+        Your Shopping Cart
+      </Typography>
+      {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
+    </Container>
+  );
+};
+
+export default Cart;
---
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import {
+  Typography,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+} from "@material-ui/core";
+
+import useStyles from "./styles";
+
+const CartItem = ({ item, handleUpdateCartQty, handleRemoveFromCart }) => {
+  const classes = useStyles();
+
+  return (
+    <Card>
+      <CardMedia
+        image={item.media.source}
+        alt={item.name}
+        className={classes.media}
+      />
+      <CardContent className={classes.cardContent}>
+        <Typography variant="h4">{item.name}</Typography>
+        <Typography variant="h5">
+          {item.line_total.formatted_with_symbol}
+        </Typography>
+      </CardContent>
+      <CardActions classname={classes.cardActions}>
+        <div className={classes.buttons}>
+          <Button
+            type="button"
+            size="small"
+            onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}
+          >
+            -
+          </Button>
+          <Button
+            type="button"
+            size="small"
+            onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}
+          >
+            +
+          </Button>
+        </div>
+        <Button
+          variant="contained"
+          type="button"
+          color="secondary"
+          onClick={() => handleRemoveFromCart(item.id)}
+        >
+          Remove
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default CartItem;
