abcbfcee360d847bb9782a20d6d5ff8440ca92a1
tybens <tb19@princeton.edu>
2021-03-25 17:29:49 -0700

    Add intro video and basic transition

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { CssBaseline } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Shop, Home } from './pages';
+import VideoIntro from './components/VideoIntro';
 
 const App = () => {
 
+  // when doing nested routing, don't make the <Route /> "exact"
   return (
     <Router>
       <div style={{ display: "flex" }}>
         <CssBaseline />
+        <VideoIntro />
         <Switch>
           <Route path="/shop">
             <Shop />
