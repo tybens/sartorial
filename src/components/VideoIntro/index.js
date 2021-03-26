d79d2c968d6fcb98c666f6c4d882001066489de5
tybens <tb19@princeton.edu>
2021-03-26 12:35:25 -0700

    Page load animatoin after video preloader finished

--- src/components/VideoIntro/index.js
+++ src/components/VideoIntro/index.js
@@ -1,37 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import { CSSTransition } from 'react-transition-group'
+import React from 'react'
 import "./transition-styles.css";
 import makeStyles from './styles'
 
 const VideoIntro = () => {
-    const [introComplete, setIntroComplete] = useState(false)
     const classes = makeStyles();
 
-    useEffect(() => {
-        if (!introComplete) {
-            const timer = setTimeout(() => {
-                setIntroComplete(true);
-            }, 5000);
-
-            return () => clearTimeout(timer);
-        }
-    });
-
     return (
-
-        <CSSTransition
-            in={!introComplete}
-            timeout={300}
-            classNames={"videoPreloader"}
-            unmountOnExit
-            appear
-        >
             <div className={classes.videoPreloader}>
                 <video width="100%" height="100%" autoPlay="autoplay" muted loop playsInline>
                     <source src="/videos/temp_intro.mp4" type="video/mp4" />
                 </video>
             </div>
-        </CSSTransition>
 
     )
 }
