bbebf57c0ab1e4b426ca0741880c7d87ebfcca0b
tybens <tb19@princeton.edu>
2021-03-26 17:21:57 -0700

    Small changes to cart view
    * Align items center
    
    * Change card to a mui grid-based card thingy

--- src/pages/Shop/Cart/CartItem/styles.js
+++ src/pages/Shop/Cart/CartItem/styles.js
@@ -1,18 +1,29 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export default makeStyles(() => ({
-  media: {
-    height: 260,
-  },
-  cardContent: {
-    display: "flex",
-    justifyContent: "space-between",
-  },
-  cartActions: {
-    justifyContent: "space-between",
-  },
+export default makeStyles((theme) => ({
   buttons: {
     display: "flex",
     alignItems: "center",
+  },
+  content: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    border: "",
+    margin: 'auto',
+    maxWidth: 500,
+    heigth: "150px",
+    width: "500px",
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
   },
 }));
