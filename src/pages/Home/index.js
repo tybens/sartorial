9bb68c1e4b65b00a2bb6592b93b264d04bf8727b
tybens <tb19@princeton.edu>
2021-03-27 17:27:46 -0700

    Add rotary gif placeholder and filler info to homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,12 +3,37 @@ import React from 'react';
 const Home = () => {
     return (
         <>
+        {/* <img src="/images/spinningrotary.gif" alt="Spinning Sartorial Logo" /> */}
         <h1>
             <div>Handcrafted <span className="with--accent">threads</span></div>
             <div>to build <span className="with--accent">habitats</span></div>
             <div>for those who</div>
             <div className="with--accent">need it most</div>
         </h1>
+        <div className="content-block block-1">
+            <h1>
+                This shows more content
+            </h1>
+        </div>
+
+        <div className="content-block block-3">
+            <h1>
+                This shows even more
+            </h1>
+        </div>
+        <div className="content-block block-0">
+            <h1>
+                This shows more content
+            </h1>
+        </div>
+        <div className="content-block block-2">
+            <h1>
+                This shows even more
+            </h1>
+        </div>
+
+
+
         </>
     );
 }
