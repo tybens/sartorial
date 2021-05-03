629dbdb3dc62c48684fd040341e7424d31fd2141
tybens <tb19@princeton.edu>
2021-05-02 20:02:24 -0400

    Fix firebase explicit url routing hopefully

--- src/App.js
+++ src/App.js
@@ -223,7 +223,7 @@ const App = () => {
                                 return null;
                               }}
                             />
-                            <Route path="/businessplan">
+                            <Route exact path="/businessplan">
                               <BusinessPlan />
                             </Route>
                           </Switch>
