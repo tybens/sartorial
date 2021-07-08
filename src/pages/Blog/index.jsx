7f308cd908620f911a30c0fb74b32a0560d60b28
tybens <tb19@princeton.edu>
2021-07-08 12:52:05 -0700

    Initial blog card and remove dib

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,13 +1,16 @@
 import React  from "react";
 import { Grid } from "@material-ui/core";
 
+import BlogCard from "./BlogCard"
 // import useStyles from "./styles.js";
 
 const Blog = () => {
   // const classes = useStyles();
   return (
     <Grid container justify-content="flex-start" direction="column">
-      <div id="dib-posts" ></div>
+      {[...Array(5)].forEach((i) => (
+        <BlogCard />
+      ))}
     </Grid>
   );
 };
