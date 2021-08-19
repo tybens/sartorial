b98985233bbdc5b0ef99b31dbc691d1792b51b7a
tybens <tb19@princeton.edu>
2021-08-19 14:47:59 -0400

    Change henry vid back to other henry vid

--- src/components/HenryVid/index.jsx
+++ src/components/HenryVid/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import makeStyles from "./styles";
-import Henry from "assets/vids/henryvid.mov";
+import Henry from "assets/vids/henryvid.mp4";
 
 const HenryVid = () => {
   const classes = makeStyles();
