3726f53416d616fadd509af36689b3a91aafd7fe
tybens <tb19@princeton.edu>
2022-02-15 19:13:31 -0500

    Artist collection placeholder page updates

--- src/pages/Shop/ArtistSample/index.jsx
+++ src/pages/Shop/ArtistSample/index.jsx
@@ -1,32 +1,110 @@
 import { Grid, Typography } from "@material-ui/core";
 import artistCollectionData from "data/artist-collection-data";
 import Products from "../Products/Products";
+import { Switch, Route, withRouter, NavLink } from "react-router-dom";
 
-const ArtistSample = () => {
+import useStyles from "./styles";
+
+const ArtistSample = ({ match }) => {
+  const classes = useStyles();
+
   return (
-    <Grid>
-      <Grid
-        item
-        container
-        justify="center"
-        alignItems="center"
-        direction="column"
-      >
-        <Grid item>
-          <Typography variant="h2">Your Artist Name Here</Typography>
+    <div>
+      <Switch>
+        <Grid
+          className={classes.container}
+          container
+          alignItems="center"
+          direction="column"
+        >
+          <Grid item container className={classes.titleContainer}>
+            <Grid item xs={12}>
+              <Typography variant="h2" align="center">
+                Your Artist Name Here
+              </Typography>
+
+              <Typography variant="body1" align="center">
+                Support my music while you support a cause.
+              </Typography>
+            </Grid>
+          </Grid>
+          <Route exact path={`${match.path}/products`}>
+            <Products
+              collectionsData={artistCollectionData}
+              collectionId="artist-sample"
+            />
+          </Route>
+          <Route exact path={`${match.path}`}>
+            <Grid
+              item
+              container
+              justify="center"
+              xs={12}
+              className={classes.productsContainer}
+            >
+              <NavLink
+                strict
+                exact
+                to={`${match.path}/products`}
+                className={classes.productsLink}
+              >
+                Products
+              </NavLink>
+            </Grid>
+            <Grid item container justify="space-around" direction="row">
+              <Grid
+                item
+                container
+                justify="center"
+                xs={4}
+                className={classes.artistPhoto}
+                style={{ borderRight: "3px dotted black" }}
+              >
+                <Typography
+                  variant="h3"
+                  color="inherit"
+                  className={classes.subTitle}
+                  style={{ color: "white" }}
+                >
+                  Artist Photo
+                </Typography>
+              </Grid>
+              <Grid
+                item
+                container
+                justify="center"
+                xs={4}
+                style={{ borderRight: "3px dotted black" }}
+              >
+                <Typography
+                  variant="h3"
+                  color="inherit"
+                  className={classes.subTitle}
+                >
+                  Artist Bio
+                </Typography>
+              </Grid>
+              <Grid
+                item
+                container
+                justify="center"
+                xs={4}
+                className={classes.trioContainer}
+              >
+                <Typography
+                  variant="h3"
+                  color="inherit"
+                  className={classes.subTitle}
+                >
+                  Recent Work
+                </Typography>
+              </Grid>
+            </Grid>
+          </Route>
         </Grid>
-        <Grid item>
-          <Typography variant="body1">Your artist description here.</Typography>
-        </Grid>
-      </Grid>
-      <Grid item>
-        <Products
-          collectionsData={artistCollectionData}
-          collectionId="artist-sample"
-        />
-      </Grid>
-    </Grid>
+      </Switch>
+    </div>
   );
 };
 
-export default ArtistSample;
+export default withRouter(ArtistSample);
