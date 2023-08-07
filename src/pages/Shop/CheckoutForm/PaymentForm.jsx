101418099c59f22bdbaa2c249e534443ce332f5a
tybens <tb19@princeton.edu>
2023-08-06 23:14:22 -0700

    a bit closer to stripe integration

--- src/pages/Shop/CheckoutForm/PaymentForm.jsx
+++ src/pages/Shop/CheckoutForm/PaymentForm.jsx
@@ -10,15 +10,16 @@ import {
   useElements,
   useStripe,
   ExpressCheckoutElement,
+  CardElement,
 } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import useStyles from "./styles";
 import Review from "./Review";
 
 const stripePromise = loadStripe(
-  "pk_live_51IBADsLCfTcKYCUjbICBBDL4ix1PYw36eqqQtagck0wc5mqXaNqVy02xeeRjESwb2cBvc1vSBN8PNkXQ3P8IcZ4V004Iznuds0"
+  "pk_test_51IBADsLCfTcKYCUjY9VNuZhMaWbppZcYBrWtC31hmMADcFH8PfzP29k7wFYZnAC8D9kQTPKUEcbHMW9OCzSJq99W00Ypdf94EK"
 );
 
 const PaymentForm = ({
@@ -29,6 +30,7 @@ const PaymentForm = ({
   onCaptureCheckout,
   totalItems,
   donation = false,
+  history,
 }) => {
   const [discount, setDiscount] = useState(0);
   const [pickup, setPickup] = useState(false);
@@ -42,9 +44,6 @@ const PaymentForm = ({
     amount: 1099,
     currency: "usd",
     // Customizable with appearance API.
-    appearance: {
-      /*...*/
-    },
   };
 
   return (
@@ -64,6 +63,7 @@ const PaymentForm = ({
       </Typography>
       <Elements stripe={stripePromise} options={options}>
         <StripePayment
+          history={history}
           cart={cart}
           pickup={pickup}
           onCaptureCheckout={onCaptureCheckout}
@@ -85,52 +85,59 @@ const StripePayment = ({
   shippingData,
   discount,
   pickup,
+  history,
 }) => {
   const [succeeded, setSucceeded] = useState(false);
-  const [error, setError] = useState(null);
-  const [processing, setProcessing] = useState("");
+  const [error, setError] = useState(false);
+  const [processing, setProcessing] = useState(false);
+  const [disabled, setDisabled] = useState(true);
   const stripe = useStripe();
   const elements = useElements();
   // const functionSecretUrl = 'http://localhost:5001/sartorial-indy/us-central1/paymentSecret'
   const functionSecretUrl =
     "https://us-central1-sartorial-indy.cloudfunctions.net/paymentSecret";
-  const navigate = useNavigate();
 
   const classes = useStyles();
 
   useEffect(() => {
     // empty cart, so navigate to home
     if (Object.keys(cart).length === 0) {
-      navigate("/");
+      history.push(`/`);
     }
-  }, [cart, navigate]);
+    // eslint-disable-next-line
+  }, [cart]);
 
-  // const cardStyle = {
-  //   style: {
-  //     base: {
-  //       color: "#32325d",
-  //       fontFamily: "Arial, sans-serif",
-  //       fontSmoothing: "antialiased",
-  //       fontSize: "16px",
-  //       "::placeholder": {
-  //         color: "#32325d",
-  //       },
-  //     },
-  //     invalid: {
-  //       color: "orange",
-  //       iconColor: "orange",
-  //     },
-  //   },
-  // };
+  const cardStyle = {
+    style: {
+      base: {
+        color: "#32325d",
+        fontFamily: "Arial, sans-serif",
+        fontSmoothing: "antialiased",
+        fontSize: "16px",
+        "::placeholder": {
+          color: "#32325d",
+        },
+      },
+      invalid: {
+        color: "orange",
+        iconColor: "orange",
+      },
+    },
+  };
 
-  // const handleChange = async (event) => {
-  //   // Listen for changes in the CardElement
-  //   // and display any errors as the customer types their card details
-  //   setDisabled(event.empty);
-  //   setError(event.error ? event.error.message : "");
-  // };
+  const handleChange = async (event) => {
+    // Listen for changes in the CardElement
+    // and display any errors as the customer types their card details
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
 
-  const onConfirm = async (event) => {
+    console.log(processing);
+    console.log(disabled);
+    console.log(error);
+  };
+
+  const onConfirm = async (event, express) => {
+    event.preventDefault();
     setProcessing(true);
 
     if (!stripe) {
@@ -158,8 +165,8 @@ const StripePayment = ({
       )
       .then(function (response) {
         // handle success
-        setSucceeded(true)
-        return { client_secret: response.data.clientSecret };
+        setSucceeded(true);
+        return response.data.clientSecret;
       })
       .catch(function (error) {
         // handle error
@@ -167,26 +174,41 @@ const StripePayment = ({
         setError(`Internal payment error ${error.message}`);
       });
 
-    const { client_secret: clientSecret } = await res.json();
+    const clientSecret = await res;
 
     // Confirm the PaymentIntent using the details collected by the Express Checkout Element
-    const { thisError } = await stripe.confirmPayment({
-      // `elements` instance used to create the Express Checkout Element
-      elements,
-      // `clientSecret` from the created PaymentIntent
-      clientSecret,
-      confirmParams: {
-        return_url: "https://example.com/order/123/complete",
-      },
-    });
+    var payload = null;
+    if (express) {
+      // if express call confirm Payment
+      payload = await stripe.confirmPayment({
+        // `elements` instance used to create the Express Checkout Element
+        elements,
+        // `clientSecret` from the created PaymentIntent
+        clientSecret,
+        confirmParams: {
+          return_url: "https://example.com/order/123/complete",
+        },
+      }) 
+    } else {
+      // otherwise call stripe.confirmCardPayment
+      payload = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      });
+    }
 
-    if (thisError) {
+    if (payload.error) {
       // This point is only reached if there's an immediate error when
       // confirming the payment. Show the error to your customer (for example, payment details incomplete)
-      setError(thisError.message);
+      setError(error.message);
+      setProcessing(false);
     } else {
       // The payment UI automatically closes with a success animation.
       // Your customer is redirected to your `return_url`.
+      setError(null);
+      setProcessing(false);
+      setSucceeded(true);
 
       let orderData = {
         cart: cart,
@@ -217,14 +239,19 @@ const StripePayment = ({
 
       // log order to database
       onCaptureCheckout(orderData);
-      setProcessing(false)
+      setProcessing(false);
       nextStep();
     }
   };
 
   return (
-    <div className={classes.paymentForm}>
+    <form className={classes.paymentForm} onSubmit={onConfirm}>
       <ExpressCheckoutElement onConfirm={onConfirm} />
+      <CardElement
+        className={classes.cardElement}
+        options={cardStyle}
+        onChange={handleChange}
+      />
       <br /> <br />
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         <Button variant="outlined" color="secondary" onClick={backStep}>
@@ -234,9 +261,9 @@ const StripePayment = ({
           type="submit"
           variant="contained"
           className={
-            processing || succeeded ? classes.disabled : ""
+            processing || disabled || succeeded ? classes.disabled : ""
           }
-          disabled={!succeeded}
+          disabled={disabled}
           id="submit"
           color="primary"
         >
@@ -255,8 +282,8 @@ const StripePayment = ({
           {error}
         </div>
       )}
-    </div>
+    </form>
   );
 };
 
-export default PaymentForm;
+export default withRouter(PaymentForm);
